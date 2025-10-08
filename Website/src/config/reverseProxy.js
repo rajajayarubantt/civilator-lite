7ba45461e71f24920a949e2f0152604a1be39551
rubandeveloper <rajajayarubant@gmail.com>
2025-10-08 22:50:12 +0530

    build

--- Website/src/config/reverseProxy.js
+++ Website/src/config/reverseProxy.js
@@ -1,6 +1,6 @@
 
 export default {
 
-    'serverBaseUrl': `http://localhost:5000/api/v1/public`,
+    'serverBaseUrl': `/api/v1/public`,
     "appBaseUrl": `https://app.civilator.aiseowrite.in`,
 }
